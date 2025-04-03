6ffd429acc74e8807e86968be4a77190c282ba01
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-03 14:33:21 +0545

    Updated

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-// console.log("Hello World!");
-// variable decleration in javascript
-// we can decleare the variable using the let and var keyword
-// example
-var a = 10;
-let b = 20;
-// now days the var has become old and we usually use let keyword to decleare variables
-// so for displaying the output on the screen can be done by using the console.log keyword
-console.log("a="+a);
-console.log("b="+b);
-// now simply initializing the a string variable
-let string = "prabin";
-//simply displaying it 
-console.log("My name is "+string); 
-//for boolean datatypes we use
- let Boolean =true;
-console.log(Boolean);
-// if we are declearing a constant variable then we can simply use const keyword,this cannnot bbe changed further.
-const age = 20;
-//age = 30; This is wrong
-// we can make object in javascripgt which makes a container where we can store multiple data of different datatype
-// we generally decleare the object by using const keywird,example
-const Student = {
-     fullname:"prabin Thapa",
-     age:20,
-     address:chitwan,
-     ispass:true
-};
+function oddeven() {
+    let num = document.getElementById("evenOdd").value;
+    let result = document.getElementById("evenOddResult");
+    if(num%2==0)
+    {
+        result.innerHTML = `${num} is Even`;
+    }
+    else{
+        result.innerHTML = `${num} is Odd`;
+    }
+}
+
+function prime() {
+    let num = document.getElementById("primeNum").value;
+    let result = document.getElementById("primeResult");
+    for (let i = 1; i <=num; i++) {
+        let count = 0;
+        if (num % i == 0) {
+            count++;
+        }
+    }
+    if(count==2)
+    {
+        result.innerHTML = `${num} is not a Prime number`;
+    }
+    else
+    {
+    result.innerHTML = `${num} is a Prime number`;
+    }
+}
+
+function palindrome() {
+    let num = document.getElementById("palindromeNum").value;
+    let result = document.getElementById("palindromeResult");
+    let rem;
+    let reversed = 0;
+    let original = num;
+    while (num > 0) {
+        rem = num % 10;
+        reversed = reversed * 10 + rem;
+        num = parseInt(num / 10);
+    }
+    if(original == reversed)
+    {
+        result.innerHTML = `${num} is a Palindrome`;
+    }
+    else{
+        result.innerHTML = `${num} is not a Palindrome`;
+    }
+}
